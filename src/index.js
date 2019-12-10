fad762011121bce8d69233417d281b454bc93fc7
Will Drougas <wdrougas@gmail.com>
2019-12-10 16:05:33 -0500

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-
   let formEl = document.querySelector("form")
 
   formEl.addEventListener("submit", createNewTask )
@@ -13,16 +11,27 @@ function createNewTask(event) {
    console.log("You are in the createnew task method")
    let taskDesc = document.getElementById("new-task-description").value
    addTodo(taskDesc)
-
 }
 
 function addTodo(taskDesc) {
     let listEl = document.createElement("li")
     let unordedList = document.querySelector("ul") 
     unordedList.appendChild(listEl)
+    listEl.id = `task${Math.random().round()}`;
     listEl.innerText = taskDesc
+    let deleteButton = document.createElement("button")
+    listEl.appendChild(deleteButton)
+    deleteButton.innerHTML = "Delete Task"
+    deleteButton.addEventListener("click", deleteTask)
     event.target.reset()
+}
+
+
+function deleteTask(){
 
 }
 
+// button.addEventListener ("click", function() {
+//   alert("did something");
+// });
 
