0971c050232f2ba4129fdca2d61f7caa9903cd5a
github username <github email address>
2019-12-10 16:27:05 -0500

    chages made

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ function addTodo(taskDesc) {
     let listEl = document.createElement("li")
     let unordedList = document.querySelector("ul") 
     unordedList.appendChild(listEl)
-    listEl.id = `task${Math.random().round()}`;
+    // var uniqueNumber = new Date().getTime();
+    // listEl.id = `task${uniqueNumber}`; 
+    listEl.id = 'task1'; 
     listEl.innerText = taskDesc
     let deleteButton = document.createElement("button")
     listEl.appendChild(deleteButton)
@@ -28,10 +30,8 @@ function addTodo(taskDesc) {
 
 
 function deleteTask(){
-
+  let deleteTask = document.getElementById("task1") 
+  deleteTask.style.display = 'none';
 }
 
-// button.addEventListener ("click", function() {
-//   alert("did something");
-// });
 
