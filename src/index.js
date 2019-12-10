44a5ca228c469ebea70cd7cbc2fe148bf8b68d44
github username <github email address>
2019-12-10 15:27:50 -0500

    initial changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let formEl = document.querySelector("form")
+
+  formEl.addEventListener("submit", createNewTask )
+
 });
+
+
+function createNewTask(event) {
+   event.preventDefault() 
+   console.log("You are in the createnew task method")
+   let taskDesc = document.getElementById("new-task-description").value
+   addTodo(taskDesc)
+
+}
+
+function addTodo(taskDesc) {
+    let listEl = document.createElement("li")
+    let unordedList = document.querySelector("ul") 
+    unordedList.appendChild(listEl)
+    listEl.innerText = taskDesc
+    event.target.reset()
+
+}
+
+
